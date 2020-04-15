017a0151ced57f68bfeac9467e2458183cc184e2
waxfish <waxfish@users.noreply.github.com>
2020-04-15 11:53:01 -0500

    123

---
+++ john_hopkings_data copy.R
@@ -0,0 +1,10 @@
+install.packages("coronavirus")
+library(tidyverse)
+library(coronavirus)
+
+unique(coronavirus %>%
+  filter(Country.Region == 'US') %>%
+  select(type))
+
+data(coronavirus)
+colnames(coronavirus)
