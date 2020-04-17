8c5be16b5cdd229a3810a162eb430bd14d1d85e7
Camilo Arias <cariasmartelo@uchicago.edu>
2020-04-16 21:14:07 -0500

    Authomatic download

--- john_hopkings_data.R
+++ john_hopkings_data.R
@@ -1,23 +1,31 @@
 rm(list = ls())
 library(tidyverse)
+library(httr)
+#setwd("~/Google Drive/Escuela/MSCAPP/Q6/covid_gov_responses/covid_cases_and_deaths")
 
-setwd("~/Google Drive/Escuela/MSCAPP/Q6/covid_gov_responses/covid_cases_and_deaths")
+# URL of updated data
+repository <- 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/'
+cases_us_csv <- 'time_series_covid19_confirmed_US.csv'
+cases_world_csv <- 'time_series_covid19_confirmed_global.csv'
+deaths_us_csv <- 'time_series_covid19_deaths_US.csv'
+deaths_world_csv <- 'time_series_covid19_deaths_global.csv'
 
-time_series_data_dir <- 'COVID-19/csse_covid_19_data/csse_covid_19_time_series'
+# Downloading most recent CSV's
+for (panel in c(cases_us_csv, cases_world_csv, deaths_us_csv, deaths_world_csv)){
+  url_path <- file.path(repository, panel)
+  filepath <- "data"
+  GET(url_path, write_disk(file.path(filepath, panel), overwrite=TRUE))
+}
 
 
-# Loading world cases
-world_cases <- read_csv(
-  file.path(time_series_data_dir, 'time_series_covid19_confirmed_global.csv'))
+# Loading cases
+cases_us <- read_csv(file.path(filepath, cases_us_csv))
+cases_world <- read_csv(file.path(filepath, cases_world_csv))
+deaths_us <- read_csv(file.path(filepath, deaths_us_csv))
+deaths_world <- read_csv(file.path(filepath, deaths_world_csv))
 
-# Loading US cases
-us_cases <- read_csv(
-  file.path(time_series_data_dir, 'time_series_covid19_confirmed_us.csv'))
 
-states_and_territories <- us_cases %>%
-  distinct(Province_State)
-
-cases_by_state <- us_cases %>%
+cases_by_state <- cases_us %>%
   select(-UID, -iso2, -iso3, -code3, -FIPS, -Admin2,
          -Country_Region, -Lat, -Long_, -Combined_Key) %>%
   group_by(Province_State) %>%
